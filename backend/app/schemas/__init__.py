75e32ef3a5bb26c53fd944efbfdeb2f54ada35be
suraj1045 <surajv1045@gmail.com>
2025-12-04 12:21:21 +0530

    created endpoints for user handling, wrote basic streamlit code

---
+++ backend/app/schemas/__init__.py
@@ -0,0 +1,19 @@
+from .user import (
+    UserBase,
+    UserCreate,  # Add this line
+    UserUpdate,
+    UserInDBBase,
+    User,
+    UserInDB,
+    Token
+)
+
+__all__ = [
+    'UserBase',
+    'UserCreate',  # Add this line
+    'UserUpdate',
+    'UserInDBBase',
+    'User',
+    'UserInDB',
+    'Token'
+]
